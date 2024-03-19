12e16a180b0cd39a104a97c76bcf32fa013f0f3c
borisngong <borisngong@gmail>
2024-03-18 18:16:54 -0700

    Update def do_create(self, arg) to allow creation of params

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from datetime import datetime
 
 
 class HBNBCommand(cmd.Cmd):
@@ -73,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,17 +115,29 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
+        """ Base on User input, this creates an object of any class"""
+        try:
+            if not args:
+                raise SyntaxError()
+            input_line = args.split(" ")
+            k_words = {}
+            for arg in input_line[1:]:
+                user_input_parts = arg.split("=")
+                if len(user_input_parts) >= 2:
+                    user_input_parts[1] = eval(user_input_parts[1])
+                    if type(user_input_parts[1]) is str:
+                        user_input_parts[1] = user_input_parts[1].replace("_", " ").replace('"', '\\"')
+                    k_words[user_input_parts[0]] = user_input_parts[1]
+
+        except SyntaxError:
             print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
+        except NameError:
             print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        if 'updated_at' not in k_words:
+            k_words['updated_at'] = datetime.now()
+        created_instace = HBNBCommand.classes[input_line[0]](**k_words)
+        created_instace.save()
+        print(created_instace.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +285,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +293,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
