61869a31782484f7f0c65885a0059edd5493626c
borisngong <borisngong@gmail>
2024-03-18 21:21:28 -0700

    A script that prepares a MySQL server for the project

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,20 +1,12 @@
 -- Script to create a MySQL server
 -- Create database hbnb_test_db
-
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
 -- Create the user if it doesn't exist
-
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
 -- Grant privileges to the user on the database
-
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
-
 -- Grant SELECT privilege on performance_schema
 
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
 -- Apply changes immediately without needing to restart MySQL
-
 FLUSH PRIVILEGES;
