3e094100c53c54e35d46b45d0cc919622b58abd6
borisngong <borisngong@gmail>
2024-04-19 11:53:03 -0700

    A script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""A module for starting flask web Application"""
+
+
+from flask import Flask
+
+"""Create a Flask application Instance"""
+app = Flask(__name__)
+"""Route decorator for the Root URL"""
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """When accessing root url, Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    """Start the Flask Application on 0.0.0.0:5000"""
+    app.run(host='0.0.0.0', port=5000)
