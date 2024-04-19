65597dc1e2f24404092922cbfb66a52bba66d1d5
borisngong <borisngong@gmail>
2024-04-19 16:24:39 -0700

    A script that starts a Flask web application: Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""A module for starting Flask web Application"""
+
+
+from flask import Flask
+from werkzeug.exceptions import HTTPException
+
+
+app = Flask(__name__)
+"""Creation of the Flask instance"""
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Route decorator for root URL"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route decorator for /hbnb URL"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_app_route(text):
+    """Route decorator for /c/<text> URL"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_app_route(text):
+    """Route decorator for /python/<text> URL"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_app_route(n):
+    """Route decorator for /number/<n> URL"""
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    """Start Flask web framework app on 0.0.0.0:5000"""
+    app.run(host='0.0.0.0', port=5000)
